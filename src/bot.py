960e9aef3c5e3b9cbee321c5ae14cbdef2a2cfe1
stasastapov <elmoons@yandex.ru>
2024-12-04 23:58:58 +0300

    fix

--- src/bot.py
+++ src/bot.py
@@ -3,16 +3,24 @@ import logging
 import sys
 
 from aiogram import Bot, Dispatcher
-
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
+from aiogram.fsm import state
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.state import State, StatesGroup
+
 from src.config import settings
 from src.parse import get_data_about_product, get_spuid
 from src.utils import check_is_admin
 
-dp = Dispatcher()
+dp = Dispatcher(storage=MemoryStorage())
+
+
+class AddProductStates(StatesGroup):
+    link_poizon_product = State()
 
 
 @dp.message(CommandStart())
@@ -25,33 +33,30 @@ async def command_start_handler(message: Message):
 
 
 @dp.message(Command(commands="add_poizon_product"))
-async def message_about_poizon_link(message: Message):
+async def message_about_poizon_link(message: Message,  state: FSMContext):
     if check_is_admin(message.chat.id):
+        await state.set_state(AddProductStates.link_poizon_product)
         await message.answer("Пришлите ссылку на товар.")
     else:
         await message.answer(f"You can't use this bot.")
 
 
-@dp.message(Command(commands="add_taobao_product"))
-async def message_about_taobao_link(message: Message):
-    if check_is_admin(message.chat.id):
-        await message.answer("Пришлите ссылку на товар.")
-    else:
-        await message.answer(f"You can't use this bot.")
+@dp.message(AddProductStates.link_poizon_product)
+async def handle_poizon_link(message: Message, state: FSMContext):
+    try:
+        spuid = get_spuid(message.text)
+        data = get_data_about_product(spuid)
+        print(data)
+        await message.answer(f"Данные товара с Poizon получены.")
+    except KeyError:
+        await message.answer("Некорректная ссылка. Не удалось получить SpuId товара.")
+    finally:
+        await state.clear()
 
 
 @dp.message()
-async def add_product(message: Message):
-    if check_is_admin(message.chat.id):
-        try:
-            spuid = get_spuid(message.text)
-            data = get_data_about_product(spuid)
-            print(data)
-            await message.answer("ye")
-        except KeyError:
-            await message.answer("Некорректная ссылка. Не удалось получить SpuId товара.")
-    else:
-        await message.answer(f"You can't use this bot.")
+async def handle_unknown_message(message: Message):
+    await message.answer("Я не понимаю это сообщение. Пожалуйста, используйте команды из меню.")
 
 
 async def main() -> None:
