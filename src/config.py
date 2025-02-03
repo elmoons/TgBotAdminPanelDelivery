2448940bdb886f6c8bba2a9ad8faea9629bcb856
stasastapov <elmoons@yandex.ru>
2025-02-03 15:28:32 +0300

    start adding db

--- src/config.py
+++ src/config.py
@@ -7,6 +7,16 @@ class Settings(BaseSettings):
     ADMIN_TG_IDS: str
     SPREADSHEET_URL: str
 
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DB_URL(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
     class Config:
         env_file = "../.env"
 
---
+++ src/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from src.config import settings
+
+engine = create_async_engine(settings.DB_URL)
+
+async_session_maker = async_sessionmaker()
